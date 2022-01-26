bad9de44f73a03e2638bf634681816ebf374ee16
CapsAmerica <98449634+CapsAmerica@users.noreply.github.com>
2022-01-26 12:49:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:100
+  });
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bDoyN-yWX/model.json ', modelLoaded);
+
+function modelLoaded() {
+console.log('Model Loaded! ');
+}
+
+function check()
+{
+  img = document.getElementById('captured Image');
+  classifier.classify(img, gotResult);
+}
+
+function gotresult(error, result) {
+  if (error) {
+    console.error(error);
+  }else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(1);
+  }
+}
